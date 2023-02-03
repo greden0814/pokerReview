c5d62670361688c9914730a13c5feb1829101ac7
greden0814 <agredenbiubiu814@163.com>
2023-02-04 00:19:40 +0800

    转牌步骤

--- pages/flopPage/flopPage.vue
+++ pages/flopPage/flopPage.vue
@@ -9,7 +9,18 @@
 			</view>
 		</view>
 		<view class="steps">
-			
+			<view class="" v-for="step in steps">
+				<!-- <view class="title">{{stepChoices[step.choose]}}</view> -->
+				<radio-group @change="radioChange">
+					<label class="choiceStyle" v-for="(choice, index) in stepChoices" :key="choice">
+						<view>
+							<radio :value="choice" :checked="index === currentChoice" />
+						</view>
+						<view>{{choice}}</view>
+					</label>
+				</radio-group>
+				<input class="uni-input" v-model="step.bid" placeholder="下注金额" />
+			</view>
 		</view>
 		<view class="cardTypeChosen" v-show="typeListVisible">
 			<cover-image class="cardType" v-for="item in [0,1,2,3]" :src="`../../static/image/${item}.png`"
@@ -33,11 +44,16 @@
 				numListVisible: false,
 				cardTypes: data.cardType,
 				cardNum: data.cardNum,
+				stepChoices: data.stepChoices,
 				cards: [],
+				settingCard: null,
+				chosenType: null,
 				flopDeck: {
 					cards: [],
 					bid: []
 				},
+				steps: [],
+				currentChoice: 0,
 			}
 		},
 		onReady() {
@@ -45,6 +61,12 @@
 				this.cards.push({
 					type: "",
 					num: "",
+				})
+			}
+			for (let i = 0; i < 3; i++) {
+				this.steps.push({
+					choose: 0,
+					bid: 0,
 				})
 			}
 		},
@@ -56,21 +78,29 @@
 			},
 			choseType(type) {
 				this.typeListVisible = false
-				this.numListVisible = true
+				this.numListVisible = true 
 				this.chosenType = type
 			},
 			choseNum(num) {
-				this.$set(this.cards[this.settingSeat].card, this.settingCard, {
+				this.$set(this.cards, this.settingCard, {
 					type: this.chosenType,
 					num: num
 				})
 				this.numListVisible = false
 			},
+			radioChange: function(evt) {
+				for (let i = 0; i < this.stepChoices.length; i++) {
+					if (this.stepChoices[i].value === evt.detail.value) {
+						this.current = i;
+						break;
+					}
+				}
+			},
 			goTurn() {
 				uni.navigateTo({
 					url: "/pages/turnPage/turnPage"
 				})
-			}
+			},
 		}
 	}
 </script>
@@ -85,6 +115,50 @@
 				border: 1px solid #000;
 				width: 40px;
 				margin-right: 3px;
+		}
+	}
+	.steps {
+		.choiceStyle {
+			display: flex;
+		}
+	}
+	.cardTypeChosen {
+		position: absolute;			
+		display: flex;
+		width: 220px;
+		height: 200px;
+		left: calc(50% - 100px);
+		flex-wrap: wrap;
+		top: calc(50% - 150px);
+		.cardType {
+			width: 80px;
+			margin: 10px;
+			height: 90px;
+		}
+	}
+	.cardNumChosen {
+		position: absolute;
+		display: flex;
+		width: 220px;
+		height: 200px;
+		left: calc(50% - 93px);
+		flex-wrap: wrap;
+		top: calc(50% - 258px);
+		.cardNum {
+			width: 40px;
+			margin: 10px;
+			height: 60px;
+			text-align: center;
+			line-height: 60px;
+			border: 1px solid #000;
+		}
+		.cardNum1,.cardNum12 {
+			margin-left: 40px;
+			margin-right: 20px;
+		}
+		.cardNum2,.cardNum13 {
+			margin-left: 4px;
+			margin-right: 20px;
 		}
 	}
 }
